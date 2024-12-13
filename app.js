624932ffc45d68dcb527930ab056535c3f83f22c
Alex Kieran Wright <alex.092@live.com>
2024-12-13 16:18:05 +0000

    fixed the thumbnail loop

--- app.js
+++ app.js
@@ -11,31 +11,15 @@ const images = [
 
 console.log(images);
 
-function createThumbnails(image1, image2, image3, image4) {
-  for (i = 0; i < images.length; i++) {
-    image1 = document.createElement("img");
-    image1.src = images[0].src;
-    image1.alt = images[0].alt;
-    image1.className = "thumbnail";
+function createThumbnails(image1) {
+  for (let i = 0; i < images.length; i++) {
+    let image1 = document.createElement("img");
+    image1.src = images[i].src;
+    image1.alt = images[i].alt;
+    image1.className = "thumbnail1";
     thumbnailContainer.appendChild(image1);
 
-    image2 = document.createElement("img");
-    image2.src = images[1].src;
-    image2.alt = images[1].alt;
-    image2.className = "thumbnail";
-    thumbnailContainer.appendChild(image2);
-
-    image3 = document.createElement("img");
-    image3.src = images[2].src;
-    image3.alt = images[2].alt;
-    image3.className = "thumbnail";
-    thumbnailContainer.appendChild(image3);
-
-    image4 = document.createElement("img");
-    image4.src = images[3].src;
-    image4.alt = images[3].alt;
-    image4.className = "thumbnail";
-    thumbnailContainer.appendChild(image4);
+    console.log("hi");
   }
 }
-createThumbnails("daisy", "horse", "flowers", "reuben");
+createThumbnails();
