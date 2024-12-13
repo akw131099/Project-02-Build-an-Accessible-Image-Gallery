0ab08a5a69f3baf5f2fc0f5e4a9379cfe5bd02df
Alex Kieran Wright <alex.092@live.com>
2024-12-13 14:49:18 +0000

    finished and simplified loop, now spits out images but repeats a few times

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ console.log("test");
 const thumbnailContainer = document.getElementById("thumbnail-container");
 
 const images = [
-  { daisy: { src: "./daisy.jpg", alt: "a jack russel terrier in long grass" } },
-  { onyxHorse: { src: "./onyxhorse.png", alt: "an ornamental horse" } },
-  {
-    pinkFlowers: { src: "./pinkflowers.png", alt: "a bouqet of pink flowers" },
-  },
-  { reuben: { src: "./reubenlying.png", alt: "an alsatian lying down" } },
+  { src: "./daisy.jpg", alt: "a jack russel terrier in long grass" },
+  { src: "./onyxhorse.png", alt: "an ornamental horse" },
+  { src: "./pinkflowers.png", alt: "a bouqet of pink flowers" },
+  { src: "./reubenlying.png", alt: "an alsatian lying down" },
 ];
 
-function createThumbnails(imageArray) {
-  for (i = 0; i <= images.length; i++) {
-    let imageDaisy = images.daisy;
-    let imageHorse = images.onyxHorse;
-    let imageFlowers = images.pinkFlowers;
-    let imageReuben = images.reuben;
-    console.log(i);
+console.log(images);
+
+function createThumbnails(image1, image2, image3, image4) {
+  for (i = 0; i < images.length; i++) {
+    image1 = document.createElement("img");
+    image1.src = images[0].src;
+    image1.alt = images[0].alt;
+    image1.className = "thumbnail";
+    thumbnailContainer.appendChild(image1);
+
+    image2 = document.createElement("img");
+    image2.src = images[1].src;
+    image2.alt = images[1].alt;
+    image2.className = "thumbnail";
+    thumbnailContainer.appendChild(image2);
+
+    image3 = document.createElement("img");
+    image3.src = images[2].src;
+    image3.alt = images[2].alt;
+    image3.className = "thumbnail";
+    thumbnailContainer.appendChild(image3);
+
+    image4 = document.createElement("img");
+    image4.src = images[3].src;
+    image4.alt = images[3].alt;
+    image4.className = "thumbnail";
+    thumbnailContainer.appendChild(image4);
   }
 }
-createThumbnails();
+createThumbnails("daisy", "horse", "flowers", "reuben");
