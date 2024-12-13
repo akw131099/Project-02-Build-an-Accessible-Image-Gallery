6c8092f9e0a8c676bc574fcb0272bff9863998ed
Alex Kieran Wright <alex.092@live.com>
2024-12-13 13:18:53 +0000

    started working on a createThumbnail function with loop

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("test");
+
+let image = [
+  { src: "./daisy.jpg", alt: "a jack russel terrier in long grass" },
+  { src: "./onyxhorse.png", alt: "an ornamental horse" },
+  { src: "./pinkflowers.png", alt: "a bouqet of pink flowers" },
+  { src: "./reubenlying.png", alt: "an alsatian lying down" },
+];
+
+function createThumbnails(imageArray){
+    for (i=0; <= image.length; i++) {
+      let imageDaisy = document.createElement('img');
+      let imageHorse = document.createElement('img');
+      let imageFlowers = document.createElement('img');
+      let imageReuben = document.createElement('img');
+    }
+  }  
