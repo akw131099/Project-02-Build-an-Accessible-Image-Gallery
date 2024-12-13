635d24f4111247f9c869eb3291bdeb16f09ca763
Alex Kieran Wright <alex.092@live.com>
2024-12-13 13:50:18 +0000

    tweaked the function, now reads i properly in the console

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ console.log("test");
 const thumbnailContainer = document.getElementById("thumbnail-container");
 
 const images = [
-  { src: "./daisy.jpg", alt: "a jack russel terrier in long grass" },
-  { src: "./onyxhorse.png", alt: "an ornamental horse" },
-  { src: "./pinkflowers.png", alt: "a bouqet of pink flowers" },
-  { src: "./reubenlying.png", alt: "an alsatian lying down" },
+  { daisy: { src: "./daisy.jpg", alt: "a jack russel terrier in long grass" } },
+  { onyxHorse: { src: "./onyxhorse.png", alt: "an ornamental horse" } },
+  {
+    pinkFlowers: { src: "./pinkflowers.png", alt: "a bouqet of pink flowers" },
+  },
+  { reuben: { src: "./reubenlying.png", alt: "an alsatian lying down" } },
 ];
 
 function createThumbnails(imageArray) {
   for (i = 0; i <= images.length; i++) {
-    let imageDaisy = images.index[0];
-    let imageHorse = images.index[1];
-    let imageFlowers = images.index[2];
-    let imageReuben = images.index[3];
+    let imageDaisy = images.daisy;
+    let imageHorse = images.onyxHorse;
+    let imageFlowers = images.pinkFlowers;
+    let imageReuben = images.reuben;
+    console.log(i);
   }
 }
+createThumbnails();
